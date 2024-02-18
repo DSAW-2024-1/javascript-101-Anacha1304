32d3b947a5ae4c650f39dd16ea5664aa647d37b1
Ana Chaves <anachpe@unisabana.edu.co>
2024-02-18 13:35:27 -0500

    Final

--- solutions.js
+++ solutions.js
@@ -35,8 +35,8 @@ function findLargest(arr) {
     if (arr[i] > a){
       a = arr[i]
     }
-  return a; 
   }
+  return a; 
 
 }
 
@@ -90,12 +90,11 @@ function isPrime(n) {
   }
 
   for (let i = 2; i <= n /2; i++){
-    if (n % 1 === 0) {
+    if (n % i === 0) {
     return false;
     } 
   }
   return true;
-  
 }
 
 module.exports = {
